0b011358dffee568badabd341809b79878b928b8
mikezyeman <mike.zyeman@gmail.com>
2021-07-05 00:21:00 +0200

    Added workspace for mobile development

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  projects: [
+    '<rootDir>/apps/web',
+    '<rootDir>/apps/api',
+    '<rootDir>/libs/ui',
+    '<rootDir>/libs/utils',
+    '<rootDir>/apps/mobile',
+  ],
+};
---
+++ libs/ui/src/lib/ui.js
@@ -0,0 +1,9 @@
+import './ui.module.scss';
+export function Ui(props) {
+  return (
+    <div>
+      <h1>Welcome to ui!</h1>
+    </div>
+  );
+}
+export default Ui;
